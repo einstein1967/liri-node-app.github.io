cfb200f9d76ff1c80e7e2f6603ad2615585b0544
einstein1967 <bootcamp.ejb@gmail.com>
2019-06-16 16:38:42 -0500

    lots of fixes
    all functions work
    code looks ugly

--- liri.js
+++ liri.js
@@ -1,43 +1,109 @@
 require("dotenv").config();
+var moment = require("moment");
 var axios = require("axios");
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var userInput = process.argv;
 var command = userInput[2];
 var name = "";
 
-for (var i=3; i<userInput.length; i++) {
+for (var i = 3; i < userInput.length; i++) {
     name += userInput[i] + " ";
 }
-console.log(name);
+name = name.trim();
 
-if (command === 'concert-this') {
-    
+var concert = function () {
+    var nameUrl = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp"
+    axios.get(nameUrl).then(function (response) {
+        console.log("Venue: " + response.data[0].venue.name + ".");
+        console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.country + ".");
+        console.log("Date: " + moment(response.data[0].datetime).format('MM DD YYYY'));
+    })
+        .catch(function (error) {
+            console.log("error", error);
+        });
 }
 
-if (command === 'movie-this') {
+var movie = function () {
+    if (name === "") {
+        name = 'Mr. Nobody';
+    };
     axios.get("http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
-            // console.log(resonse);
-            console.log("The movie's rating is: " + response.data.imdbRating);
+            // console.log(response);
+            console.log("Title: " + response.data.Title);
+            console.log("Release date: " + response.data.Released);
+            console.log("IMDB rating: " + response.data.imdbRating);
+            console.log("Country where produced: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
         })
+        .catch(function (error) {
+            console.log("error", error);
+        });
+};
+
+var justDoIt = function () {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data);
+        var dataArr = data.split(",");
+        console.log(dataArr);
+        command = dataArr[0];
+        name = dataArr[1];
+        if (command === 'spotify-this-song') {
+            if (name === "") {
+                name = 'The Sign';
+            };
+            spotify.search({ type: 'track', query: name }, function (err, response) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                // console.log(response.tracks.items[0].album);
+                console.log("Artist(s): " + response.tracks.items[0].artists[0].name);
+                console.log("Song name: " + response.tracks.items[0].name);
+                console.log("Preview link: " + response.tracks.items[0].preview_url);
+                console.log("Album: " + response.tracks.items[0].album);
+            });
+        } else {
+            sortItOut();
+        }
+    });
 }
 
+var sortItOut = function () {
+    switch (command) {
+        case 'concert-this':
+            concert();
+            break;
 
-// break;
-//     case 'spotify-this-song':
-// //code
-// break;
-//     case 'movie-this':
-// axios.get('http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy').then(
-//     function (response) {
-//         console.log("The movie's rating is: " + response.data.imdbRating);
-//     });
-// break;
-//     case 'do-what-it-says':
-// //code
-// break;
+        case 'movie-this':
+            movie();
+            break;
 
-// .catch (function(error) {
-//     console.log(error)
-// });
+        case 'do-what-it-says':
+            justDoIt();
+            break;
+    };
+};
+
+if (command === 'spotify-this-song') {
+    if (name === "") {
+        name = 'The Sign';
+    };
+    spotify.search({ type: 'track', query: name }, function (err, response) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(response.tracks.items[0].album);
+        console.log("Artist(s): " + response.tracks.items[0].artists[0].name);
+        console.log("Song name: " + response.tracks.items[0].name);
+        console.log("Preview link: " + response.tracks.items[0].preview_url);
+        console.log("Album: " + response.tracks.items[0].album);
+    });
+}
