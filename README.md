29c3bfe07504153260280d03e1b9419f359a710d
einstein1967 <bootcamp.ejb@gmail.com>
2019-06-17 17:38:24 -0500

    turn in push
    fixed minor issue with spotify search
    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # liri-node-app.github.io
+
+This code will take input from the user and look up information on concerts, movies, or a song on Spotify. The last bit if functionality will read from a text file and run the above searches based on the contents of that file.
+
+The commands to enter are 'concert-this', 'movie-this, 'spotify-this-song', and 'do-what-it-says'. The first three should be followed by a music artist, movie, or song respectively. The last command needs nothing else entered by the user.
+
+Axios, moment, and Spotify npm's are used and the API keys are protected.
+
+The Spotify portion of the program is duplicated because I was having issues with the spotify.search function somehow being unrecognized.
--- liri.js
+++ liri.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
+var keys = require("./keys.js");
 var moment = require("moment");
 var axios = require("axios");
-var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var userInput = process.argv;
 var command = userInput[2];
 var name = "";
-var sort = true;
 
 for (var i = 3; i < userInput.length; i++) {
     name += userInput[i] + " ";
@@ -18,9 +17,12 @@ name = name.trim();
 var concert = function () {
     var nameUrl = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp"
     axios.get(nameUrl).then(function (response) {
+        console.log("---------------------------");
+        console.log("Artist: " + name);
         console.log("Venue: " + response.data[0].venue.name + ".");
         console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.country + ".");
         console.log("Date: " + moment(response.data[0].datetime).format('MM DD YYYY'));
+        console.log("---------------------------");
     })
         .catch(function (error) {
             console.log("error", error);
@@ -34,6 +36,7 @@ var movie = function () {
     axios.get("http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
             // console.log(response);
+            console.log("---------------------------");
             console.log("Title: " + response.data.Title);
             console.log("Release date: " + response.data.Released);
             console.log("IMDB rating: " + response.data.imdbRating);
@@ -41,6 +44,7 @@ var movie = function () {
             console.log("Language: " + response.data.Language);
             console.log("Plot: " + response.data.Plot);
             console.log("Actors: " + response.data.Actors);
+            console.log("---------------------------");
         })
         .catch(function (error) {
             console.log("error", error);
@@ -52,9 +56,9 @@ var justDoIt = function () {
         if (error) {
             return console.log(error);
         }
-        console.log(data);
+        // console.log(data);
         var dataArr = data.split(",");
-        console.log(dataArr);
+        // console.log(dataArr);
         command = dataArr[0];
         name = dataArr[1];
         if (command === 'spotify-this-song') {
@@ -66,24 +70,31 @@ var justDoIt = function () {
                     return console.log('Error occurred: ' + err);
                 }
                 // console.log(response.tracks.items[0].album);
+                console.log("---------------------------");
                 console.log("Artist(s): " + response.tracks.items[0].artists[0].name);
                 console.log("Song name: " + response.tracks.items[0].name);
                 console.log("Preview link: " + response.tracks.items[0].preview_url);
-                console.log("Album: " + response.tracks.items[0].album);
-                sort = false;
+                console.log("Album: " + response.tracks.items[0].album.name);
+                console.log("---------------------------");
             });
         } else {
-            sort = false;
-            sortItOut();
-        }
+            switch (dataArr[0]) {
+                case 'concert-this':
+                    concert();
+                    break;
+
+                case 'movie-this':
+                    movie();
+                    break;
+
+                case 'do-what-it-says':
+                    justDoIt();
+                    break;
+            };
+        };
     });
 }
 
-if (sort) {
-    sortItOut();
-}
-
-var sortItOut = function () {
     switch (command) {
         case 'concert-this':
             concert();
@@ -96,7 +107,6 @@ var sortItOut = function () {
         case 'do-what-it-says':
             justDoIt();
             break;
-    };
 };
 
 if (command === 'spotify-this-song') {
@@ -107,10 +117,12 @@ if (command === 'spotify-this-song') {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log(response.tracks.items[0].album);
+        // console.log(response.tracks.items[0].album);
+        console.log("---------------------------");
         console.log("Artist(s): " + response.tracks.items[0].artists[0].name);
         console.log("Song name: " + response.tracks.items[0].name);
         console.log("Preview link: " + response.tracks.items[0].preview_url);
-        console.log("Album: " + response.tracks.items[0].album);
+        console.log("Album: " + response.tracks.items[0].album.name);
+        console.log("---------------------------");
     });
 }
